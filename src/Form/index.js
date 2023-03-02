c0400704b3d678f52eb1de24cd9785112ff3980b
Sebastian Romanczuk <sebastian.romanczuk1999@gmail.com>
2023-03-02 19:24:34 +0100

    add new From

---
+++ src/Form/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+const Form = ({ addNewTask }) => {
+    const [formValue, setFormValue] = React.useState("");
+
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                formValue.trim() && addNewTask(formValue.trim());
+                setFormValue("");
+            }}
+            className="form"
+        >
+            <input
+                onChange={(event) => setFormValue(event.target.value)}
+                value={formValue}
+                autoFocus={true}
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    );
+};
+
+export default Form;
